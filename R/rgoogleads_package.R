5165581fcdf8c2647f15f7ef407ff2947146589c
Alexey Seleznev <selesnow@mail.ru>
2021-07-05 10:28:44 +0300

    Подкорректировал главную страницу документации пакета.

--- R/rgoogleads_package.R
+++ R/rgoogleads_package.R
@@ -1,10 +1,15 @@
 #' Loading Data From 'Google Ads API'
 #'
-#' @description \packageDescription{rgoogleads}
-#' @author \packageAuthor{rgoogleads}
+#' @description
+#' \if{html}{\figure{rgoogleads.png}{options: align='right' alt='logo' width='120'}}
+#' Interface for loading data from 'Google Ads API',
+#' see <https://developers.google.com/google-ads/api/docs/start>.
+#' Package provide function for authorization and loading reports.
+#' @author Alexey Seleznev
 #' @seealso See:
 #' \href{https://developers.google.com/google-ads/api/docs/start}{Oficial Google Ads API documantation}
 #' \href{https://developers.google.com/google-ads/api/fields/v8/overview_query_builder}{Google Ads Query Builder}
 #' @docType package
-#' @name rgoogleads
+#' @name rgoogleads-package
+#' @aliases rgoogleads
 NULL
