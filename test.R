bbc235fa812e726741ef2cca4ff38024d7f0a43b
Alexey <selesnow@gmail.com>
2021-07-17 11:37:28 +0300

    Тесты

--- test.R
+++ test.R
@@ -8,7 +8,7 @@ library(rgoogleads)
 
 # авторизация
 gads_auth_configure(path = 'D:/ga_auth/app.json')
-gads_auth(email = 'alsey.netpeak@gmail.com')
+gads_auth(email = 'alsey.netpeak@gmail.com', )
 
 
 gads_auth(email = 'selesnow.netpeak@gmail.com')
